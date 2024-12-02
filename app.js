247af43e908a6146b9c060dfe3fc5b4f017ff684
EliavC <eliavc2001@gmail.com>
2024-12-02 22:24:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 const port = process.env.PORT;
 
 
-//const postRoutes= require("./routes/posts_routes");
-//app.use("/posts",postRoutes);
+const postRoutes= require("./routes/posts_routes");
+app.use("/posts",postRoutes);
 
 const commentRoutes = require("./routes/comment_routes")
 app.use("/comments",commentRoutes)
